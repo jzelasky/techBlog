1a18a4c09a6bc1b71c7dadede427f77bfcf96f1e
Jessica Zelasky <jzelasky@gmail.com>
2023-03-23 18:01:30 -0400

    debugged logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,10 +25,9 @@ router.post('/', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+      res.status(200).json({ user: userData });
     });
   } catch (err) {
-    console.log(err)
     res.status(400).json(err);
   }
 });
@@ -63,7 +62,7 @@ router.post('/login', async (req, res) => {
 });
 
 //logouts
-router.post('/logout', (req, res) => {
+router.post('/logout', async (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
